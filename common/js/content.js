22a5daa84619a72fa66c7630604702a7a556fd44
idchoi2 <idchoi2@gmail.com>
2016-07-20 12:59:12 +0900

    16.07.20

--- common/js/content.js
+++ common/js/content.js
@@ -1,21 +1,23 @@
 "use strict";
-$(document).ready(function(){
+$(window).load(function(){
 	// 플러그인 호출
-	$(".sd2_toggle_list").toggleList();
+	//$(".sd2_toggle_list").toggleList().openItem(0);
+	$(".sd2_toggle_list").showHidden();
 	$(".sd2_forum_list").fadeList();
 	$(".sd2_support_list").fadeList();
 	$(".sd2_select_btn").selectBox();
+	$(".sd2_more_less_btn").moreLess();
 	$(".sd2_show_line").showLine(false);
 });
 
 (function($){
-	var classN="sd2_"; 
+	var classN="sd2_";
 	var active=classN+"active";
 
 	function ToggleList(selector) {
 		this.list=null;
-		this.selected=null; 
-		this.item=null; 
+		this.selected=null;
+		this.item=null;
 		this.preId=0;
 
 		this.init(selector);
@@ -29,10 +31,12 @@ $(document).ready(function(){
 
 	ToggleList.prototype.eventDefine=function(){
 		var objThis=this;
-		this.item.on("click", function(){
-			objThis.listClick($(this))
-			return false; 
+		this.item.find("."+classN+"question_wrap a").on("click", function(){// 수정
+			var clickItem=$(this).parents("li");//수정
+			objThis.listClick(clickItem);
+			return false;
 		});
+		//
 	}
 
 	ToggleList.prototype.listClick=function(item){
@@ -49,38 +53,77 @@ $(document).ready(function(){
 		this.preId=item.index();
 	}
 
+	function ShowHidden(selector){//
+		this.init(selector);//
+		this.allEvent();
+	}//
+	ShowHidden.prototype=new ToggleList();//
+	ShowHidden.prototype.allEvent=function(){
+		var thisObj=this;
+		$(".show").on("click", function(){
+			thisObj.item.find(".sd2_answer_wrap").slideDown(200);
+			thisObj.item.addClass(active);
+			return false;
+		})
+		$(".hidden").on("click", function(){
+			thisObj.item.find(".sd2_answer_wrap").slideUp(200);
+			thisObj.item.removeClass(active);
+			return false;
+		})
+	}
+
 	$.fn.toggleList=function(){
-		this.each(function(index){
+		this.each(function(selectIndex){
 			var list = new ToggleList(this);
+			$(this).data("list", list);//추가
 		})
 		return this;
 	}
 	//toggleList
 
+
+	//추가 시작
+	$.fn.openItem=function(selectIndex){
+		this.each(function(){
+			var list=$(this).data("list");
+			if(list){
+				list.listClick(list.item.eq(selectIndex))
+			}
+		})
+	}//openitem 추가 끝
+
+	$.fn.showHidden=function(){
+		this.each(function(index){
+			var list=new ShowHidden(this);
+		})
+		return this;
+	}
+
+//-------------------------------------
 	function FadeList(selector) {
 		this.forum=null;
 		this.forumList=null;
-		this.seleted=null; 
-		this.seletedBox=null; 
+		this.seleted=null;
+		this.seletedBox=null;
 		this.forumParent=null;
 		this.parentH=0;
 		this.speed=300;
 		this.fadeSpeed=150;
 		this.prevId=0;
-		this.ease="easeOutCubic"; 
+		this.ease="easeOutCubic";
 
 		this.init(selector);
 	}
 
 	FadeList.prototype.init=function(selector){
 		this.forum=$(selector);
-		this.forumList=	this.forum.children("li"); 
-		this.forumParent=$("."+classN+"fade_wrap"); 
+		this.forumList=	this.forum.children("li");
+		this.forumParent=$("."+classN+"fade_wrap");
 		this.parentH=this.forumParent.height();
 		this.eventDefine();
-	
+
 	}
-	
+
 	FadeList.prototype.eventDefine=function(){
 		var objThis=this
 		this.forumList.on("mouseover", function(e){
@@ -89,7 +132,7 @@ $(document).ready(function(){
 	}
 
 	FadeList.prototype.overFn=function(item){
-		var $this=item; 
+		var $this=item;
 		var name=$this.attr("class").replace(active, "");
 		var box=$("."+classN+"f_box");
 		var boxSelector=box.find("."+name);
@@ -97,23 +140,23 @@ $(document).ready(function(){
 		if(this.seleted){
 			if($this.index() != this.prevId){
 				this.seletedBox.stop().fadeOut(this.fadeSpeed);
-				this.seleted.removeClass(active); 
+				this.seleted.removeClass(active);
 			}
 		}
 
 		if(boxSelector.length !=0){
 			var boxH=boxSelector.outerHeight(true);
-			var nowParentH=this.parentH+boxH; 
+			var nowParentH=this.parentH+boxH;
 			boxSelector.stop().delay(100).fadeIn(this.fadeSpeed);
 			this.forumParent.stop().animate({"height": nowParentH+ "px"}, this.speed, this.ease);
 		}else{
 			this.forumParent.stop().animate({"height": this.parentH+ "px"}, this.speed, this.ease);
 		}
-		$this.addClass(active); 
-		this.seleted=$this; 
-		this.seletedBox=boxSelector; 
+		$this.addClass(active);
+		this.seleted=$this;
+		this.seletedBox=boxSelector;
 		this.prevId=$this.index();
-	
+
 	}
 
 	$.fn.fadeList=function(){
@@ -125,46 +168,94 @@ $(document).ready(function(){
 	//fadeList
 
 	function SelectBox(selector){
-		this.selectBtn=null; 
-		this.init(selector); 
+		this.selectBtn=null;
+		this.init(selector);
 	}
 
 	SelectBox.prototype.init=function(selector){
-		this.selectBtn=$(selector); 
+		this.selectBtn=$(selector);
 		this.eventDefine();
 	}
 
 	SelectBox.prototype.eventDefine=function(){
-		var objThis=this; 
+		var objThis=this;
 		this.selectBtn.on("click", function(){
-			var item=$(this); 
-			objThis.boxToggle(item); 
-			return false; 
+			var item=$(this);
+			objThis.boxToggle(item);
+			return false;
 		})
 	}
 
 	SelectBox.prototype.boxToggle=function(item){
-		var selectBox=item.next("div"); 
-		selectBox.toggle(); 
+		var selectBox=item.next("div");
+		selectBox.toggle();
 		item.toggleClass(active);
-		
+
 		selectBox.find("li").on("click", function(){
 			var selectTxt=$(this).text();
-			item.text(selectTxt).removeClass(active); 
+			item.text(selectTxt).removeClass(active);
 			selectBox.hide();
-			return false; 
+			return false;
 		})
 	}
 
 	$.fn.selectBox=function(){
 		this.each(function(index){
-			var selectForm=new SelectBox(this); 
+			var selectForm=new SelectBox(this);
 		})
-		return this; 
+		return this;
 	}
 
+	function MoreLess(selector){
+		this.moreLessBtn=null;
+		this.viewContainer=null;
+		this.init(selector);
+	}
 
+	MoreLess.prototype.init=function(selector){
+		this.moreLessBtn=$(selector).find("a") ;
+		this.viewContainer=$(selector).prev();
+		this.eventDefine();
+	}
 
+	MoreLess.prototype.eventDefine=function(){
+		var now_h=this.viewContainer.height();
+		var all_h=this.viewContainer.children("div:first")[0].scrollHeight;
+		var remove="";
+		var add="";
+		var txt="";
+		var h=0;
+		var objThis=this;
+
+		this.moreLessBtn.on("click", function(){
+			var $this=$(this);
+			if($this.hasClass(classN+"more")){
+				remove="more";
+				add="less";
+				txt="less";
+				h=all_h;
+			}else{
+				remove="less";
+				add="more";
+				txt="more";
+				h=now_h;
+			}
+
+			objThis.viewContainer.animate({"height":h+"px"}, 2000, "easeOutCubic", function(){
+				$this.removeClass(classN+remove).addClass(classN+add).text(txt);
+			});
+			return false;
+		})
+
+		//시작
+	}
+
+	$.fn.moreLess=function(){
+		this.each(function(index){
+			var moreLessBtn = new MoreLess(this);
+		})
+		return this;
+	}
 
 	function ShowLine(selector, isShowLine){
 		this.showCheckbox=null;
@@ -214,8 +305,7 @@ $(document).ready(function(){
 		})
 		return this;
 	}
+
 })(jQuery)
-
-
 
 
